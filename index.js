53da627f5538a004cfb95417f63376e9a17ecd10
wandera7 <julius.wandera@student.moringaschool.com>
2022-06-08 14:11:16 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names,event){
+    const messageCollection=[];
+  for(let i=0;i<names.length;i++){     
+      let message=`Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messageCollection.push(message);
+  }
+    return messageCollection;
+}
+function countDown(n){
+    while(n>=0){
+        console.log(n);
+        n-=1;
+    }
+}
